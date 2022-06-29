c41c0ec5de9f3e2c282418cd4d9d672c762f8be2
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2022-06-30 02:09:13 +0530

    bug : video links not working
    update video links in database also change favicon and Title of web app.

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -10,7 +10,7 @@ export const videos = [
     description:
       "neog.camp is one of its kind bootcamp where we are taking absolute beginners and getting them to a full stack web dev level in 6 months.In this session, I will be walking you through the structure of the camp. And will take questions from the live chat. If you already know about the camp, you can skip this session and save your time. Details of the camp can be found on https://neog.camp",
     creator: "Tanay Pratap",
-    link: "blob:https://www.youtube.com/f95f9ceb-ffb9-4abf-8613-dad2c2b01722",
+    link: "https://www.youtube.com/embed/Ezk2AwqgS9Q",
     pic: "https://i.ytimg.com/vi/Ezk2AwqgS9Q/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLA1VisFh6_Uurec8nadJS6rc2Mdyw",
   },
   {
@@ -19,7 +19,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's first class. This is the most basic class you'll find on programming. We will use JavaScript to code. Using nodeJS we will write a CLI (Command Line Interface) program. We'll then use https://repl.it to host it. ",
-    link: "blob:https://www.youtube.com/6d3f3493-b7fd-4d2e-86ca-7763b4c5e1f6",
+    link: "https://www.youtube.com/embed/_L-UszPmy2A",
     pic: "https://i.ytimg.com/vi/_L-UszPmy2A/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLC4-OXdTPpZdf3llkLcLVEjzl-bGg",
   },
   {
@@ -28,7 +28,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's second class. For a newbie to self host his/her first website and program more apps we need some tooling to be in place. Tools help you make your life easier. All these tools are free for you and will give you a better place to code, a place to store your code in the cloud so that you don't lose it, and then a place to host your app so that others can find it. ",
-    link: "blob:https://youtu.be/MMEIVh49pS8?list=PLzvhQUIpvvuj5KPnyPyWsvgyzNkX_ACPA",
+    link: "https://www.youtube.com/embed/MMEIVh49pS8",
     pic: "https://i.ytimg.com/vi/MMEIVh49pS8/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLCgITDTeBCwKkr2CW3zt-bt3P5uSA",
   },
   {
@@ -37,7 +37,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's levelZero journey. You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures. ",
-    link: "blob:https://youtu.be/No8qdcVYiQw?list=PLzvhQUIpvvuj5KPnyPyWsvgyzNkX_ACPA",
+    link: "https://www.youtube.com/embed/No8qdcVYiQw",
     pic: "	https://i.ytimg.com/vi/No8qdcVYiQw/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLB5Y0QC8VlGkKbJ6fjwnhOPrdkLyw",
   },
   {
@@ -46,7 +46,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Tentative Duration: 2 hours. Back after break from first session of today. This time we will do CSS. Welcome to neogcamp levelZero journey. You do not know anything about web development? Good, you have come to the right place. Not only you will learn the language of the web: HTML, you will learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures.",
-    link: "blob:https://www.youtube.com/b30e3363-a4ae-4d3b-bda7-55de47874a53",
+    link: "https://www.youtube.com/embed/u6_a0d94A1Q",
     pic: "https://i.ytimg.com/vi/u6_a0d94A1Q/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLCFbCbuzqAtwJwTRe63CQW8GAmlsw",
   },
   {
@@ -55,7 +55,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp. We have learned programming and web basics in previous lessons. This time we will use both things to create a dynamic app in a web browser. You'll learn about DOM scripting i.e. JS in the browser, calling servers, and getting data from there, taking user input and showing user output, and many more things.",
-    link: "blob:https://www.youtube.com/74e7dcf8-5e78-449b-a7f3-de0002d0f7d8",
+    link: "https://www.youtube.com/embed/yLZazznWoAs",
     pic: "https://i.ytimg.com/vi/yLZazznWoAs/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLDEXYMLowXfqVUeYVrT-OJqQDUJUA",
   },
   {
@@ -64,7 +64,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp. We have learned programming, web basics and vanillaJS in previous lessons. Now we use ReactJS, the most in-demand JS library today to create a dynamic app in the browser. We will be looking at the basics of React, JSX, useState, onClick handler, etc. ",
-    link: "blob:https://www.youtube.com/13bf8a69-8811-481c-80f3-53231c7eb26f",
+    link: "https://www.youtube.com/embed/KUJsaM-hAjs",
     pic: "https://i.ytimg.com/vi/KUJsaM-hAjs/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLCi-nZ8gs-FgCg4jsjAgmiWNZwyiA",
   },
   {
@@ -73,7 +73,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's levelZero live pair programming session.",
-    link: "blob:https://www.youtube.com/96c1fe63-253e-4765-91de-1dea3c8e7ce4",
+    link: "https://www.youtube.com/embed/oqpuG3-Pnvc",
     pic: "https://i.ytimg.com/vi/oqpuG3-Pnvc/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLAwJ1XnR8GkclWIqJahuvPOCXpviQ",
   },
   {
@@ -82,7 +82,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's levelZero live pair programming session.",
-    link: "blob:https://www.youtube.com/13479ad0-7f47-422c-af3d-4c705535ffec",
+    link: "https://www.youtube.com/embed/KKodpTaLHUI",
     pic: "https://i.ytimg.com/vi/KKodpTaLHUI/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLBdpH1oUQi_ySNcwSvEXhqNf-__SA",
   },
   {
@@ -91,7 +91,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's levelZero live pair programming session.",
-    link: "blob:https://www.youtube.com/88a02a6f-24b1-410b-a1a7-9b6674cb5b9f",
+    link: "https://www.youtube.com/embed/xOaKv-6bJwE",
     pic: "https://i.ytimg.com/vi/xOaKv-6bJwE/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLCIVU6eLN1zGpKgyazvT31V5mDDcg",
   },
   {
@@ -100,7 +100,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's levelZero live pair programming session.",
-    link: "blob:https://www.youtube.com/ca22a7dd-e989-4d13-aa79-835fd935de8c",
+    link: "https://www.youtube.com/embed/8rqZ524NzaQ",
     pic: "	https://i.ytimg.com/vi/8rqZ524NzaQ/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLCDTPZ8ZUlZhBYDeTccRnl3SE0dwA",
   },
   {
@@ -109,7 +109,7 @@ export const videos = [
     creator: "Tanay Pratap",
     description:
       "Welcome to neogcamp's levelZero live pair programming session. ",
-    link: "blob:https://www.youtube.com/3c0f2e21-5194-4584-b0ec-ff5b31c942d1",
+    link: "https://www.youtube.com/embed/P0dL7sl5jIc",
     pic: "https://i.ytimg.com/vi/P0dL7sl5jIc/hqdefault.jpg?s…j0AgKJDeAE=&rs=AOn4CLBptugyJlvptjzl-xDyk9jF-aBKAA",
   },
 ];
